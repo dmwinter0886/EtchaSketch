4ceda8fcbb304f592355632c70f37dbdafada65d
dmwinter0886 <dmwinter0886@gmail.com>
2015-03-11 16:32:44 -0500

    button
    added button, fixed grid, and refresh of grid.
    
    Still some numbers leave blank space at bottom of the container, can't
    figure that one out.

--- scripting.js
+++ scripting.js
@@ -3,19 +3,30 @@ $(document).ready(function(){
 	$('.gridCell').mouseenter(function(){
 		$(this).css("background-color", "black");
 	});
+	$('#newGridButton').click(function(){
+		$('.gridCell').remove();
+		var userNum = prompt('Type an integer between 1 and 64');
+		displayGrid(userNum);
+		$('.gridCell').mouseenter(function(){
+		$(this).css("background-color", "black");
+	});
+	});
 
 });
 
 function displayGrid(n){
-	//var userNum = prompt('Type an integer between 1 and 64');
+	
 	var cellSize = 960/n;
-	var myClass = "gridCell";
+
 	for(var i=0; i<n; i++)
 	{
 		for(var j=0; j<n; j++)
 		{
-			$('#container').append( $('<div>&nbsp</div>').addClass("gridCell").css("width",cellSize));
-			$('.gridCell').height(cellSize);
+			$('#container').append( $('<div>&nbsp</div>').addClass("gridCell"));
 		}
 	}
+	$('.gridCell').height(cellSize);
+	$('.gridCell').width(cellSize);
+	$('#container').height(cellSize*n);
+	console.log('height of containter is ' + $('#container').height() );
 }
